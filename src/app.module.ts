48912fb835d941c652b5ad4b7f4a7cc93288af77
zhaozhuang <zzzhaozzzz@163.com>
2020-01-10 16:35:14 +0800

    添加本地身份验证策略

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './api/cats/cats.modules';
 import { WsModule } from './api/websockets/ws.modules';
+import { AuthModule } from './api/auth/auth.module';
+import { UsersModule } from './api/users/users.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), CatsModule, WsModule],
+  imports: [TypeOrmModule.forRoot(), CatsModule, WsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
